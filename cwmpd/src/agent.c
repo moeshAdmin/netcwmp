d85f7dbb6593a5214cd82730cb2d644748c05ee0
peter_kuo <peter_kuo@tti.tv>
2020-08-03 16:32:13 +0800

    6

--- cwmpd/src/agent.c
+++ cwmpd/src/agent.c
@@ -465,7 +465,9 @@ int cwmp_agent_analyse_session(cwmp_session_t * session)
 
     xmlbuf = pool_palloc(doctmppool, msglength+32);
 
-    xmlbuf2 = replace(xmlbuf, "<?xml", "<cwmp");
+    xmlbuf2 = replace(xmlbuf, "xml", "cwmp");
+    cwmp_log_debug("xmlbuf2: \n%s", xmlbuf2);
+
     len = sprintf(xmlbuf2,"\0");
     cwmp_chunk_copy(xmlbuf2 + len, session->readers, msglength);
     strcpy(xmlbuf2+len+msglength, "</cwmp>");
