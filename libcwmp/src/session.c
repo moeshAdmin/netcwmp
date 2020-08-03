51f7426e31591a77eb59af6404d94272bdaa99e1
peter_kuo <peter_kuo@tti.tv>
2020-08-03 16:37:55 +0800

    update

--- libcwmp/src/session.c
+++ libcwmp/src/session.c
@@ -1136,12 +1136,12 @@ int cwmp_session_send_request(cwmp_session_t * session)
             //post empty
             //receive msg
 
-            http_post(session->sock, request, session->writers, session->envpool);
+            http_post(session->sock, request, NULL, session->envpool);
             rv = cwmp_session_recv_response(session);
         }
     }
 
-    rv = http_post(session->sock, request, NULL, session->envpool);
+    rv = http_post(session->sock, request, session->writers, session->envpool);
 
 
     if (rv <= 0)
